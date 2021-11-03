2633f914c93458ea598900b56d45cdcdab391ce1
hashimotom92 <hoge@example.com>
2021-11-04 01:33:16 +0900

    add: routetable

--- lib/network-stack.ts
+++ lib/network-stack.ts
@@ -1,9 +1,11 @@
 import * as cdk from '@aws-cdk/core';
 import {
   DefaultInstanceTenancy,
-  RouterType,
+  CfnRouteTable,
+  CfnRoute,
+  CfnSubnetRouteTableAssociation,
   Vpc,
-  Subnet,
+  CfnSubnet,
   CfnInternetGateway,
   CfnVPCGatewayAttachment
 } from "@aws-cdk/aws-ec2"
@@ -21,59 +23,111 @@ export class NetworkStack extends cdk.Stack {
       subnetConfiguration: []
     })
 
+    // IGW
+    const internetGateway = new CfnInternetGateway(this, "InternetGateway", {})
+    new CfnVPCGatewayAttachment(this, "igwAttachment", {
+      vpcId: vpc.vpcId,
+      internetGatewayId: internetGateway.ref
+    })
+
     // ingress subnet
-    new Subnet(this, `${appName}-subnet-pulic-ingress-1a`, {
+    const pubSubentIngress1A = new CfnSubnet(this, `${appName}-subnet-pulic-ingress-1a`, {
       availabilityZone: "ap-northeast-1a",
       vpcId: vpc.vpcId,
       cidrBlock: "10.0.0.0/24"
     })
-    new Subnet(this, `${appName}-subnet-pulic-ingress-1c`, {
+    const pubSubentIngress1C = new CfnSubnet(this, `${appName}-subnet-pulic-ingress-1c`, {
       availabilityZone: "ap-northeast-1c",
       vpcId: vpc.vpcId,
       cidrBlock: "10.0.1.0/24"
     })
 
     // application subnet
-    new Subnet(this, `${appName}-subnet-private-container-1a`, {
+    const priSubnetApp1A = new CfnSubnet(this, `${appName}-subnet-private-app-1a`, {
       availabilityZone: "ap-northeast-1a",
       vpcId: vpc.vpcId,
       cidrBlock: "10.0.8.0/24"
     })
-    new Subnet(this, `${appName}-subnet-private-container-1c`, {
+    const priSubnetApp1C =new CfnSubnet(this, `${appName}-subnet-private-app-1c`, {
       availabilityZone: "ap-northeast-1c",
       vpcId: vpc.vpcId,
       cidrBlock: "10.0.9.0/24"
     })
 
     // db subnet
-    new Subnet(this, `${appName}-subnet-private-db-1a`, {
+    const priSubnetDB1A = new CfnSubnet(this, `${appName}-subnet-private-db-1a`, {
       availabilityZone: "ap-northeast-1a",
       vpcId: vpc.vpcId,
       cidrBlock: "10.0.16.0/24"
     })
-    new Subnet(this, `${appName}-subnet-private-db-1c`, {
+    const priSubnetDB1C = new CfnSubnet(this, `${appName}-subnet-private-db-1c`, {
       availabilityZone: "ap-northeast-1c",
       vpcId: vpc.vpcId,
       cidrBlock: "10.0.17.0/24"
     })
 
     // management subnet
-    new Subnet(this, `${appName}-subnet-pulic-management-1a`, {
+    const pubSubentManagement1A = new CfnSubnet(this, `${appName}-subnet-pulic-management-1a`, {
       availabilityZone: "ap-northeast-1a",
       vpcId: vpc.vpcId,
       cidrBlock: "10.0.240.0/24"
     })
-    new Subnet(this, `${appName}-subnet-pulic-management-1c`, {
+    const pubSubentManagement1C = new CfnSubnet(this, `${appName}-subnet-pulic-management-1c`, {
       availabilityZone: "ap-northeast-1c",
       vpcId: vpc.vpcId,
       cidrBlock: "10.0.241.0/24"
     })
 
-    // IGW
-    const internetGateway = new CfnInternetGateway(this, "InternetGateway", {})
-    new CfnVPCGatewayAttachment(this, "igwAttachment", {
+    // Public RouteTable
+    const publicRouteTable = new CfnRouteTable(this, 'PublicRouteTable', {
       vpcId: vpc.vpcId,
-      internetGatewayId: internetGateway.ref
+    })
+    new CfnRoute(this, 'PublicRoute', {
+      routeTableId: publicRouteTable.ref,
+      destinationCidrBlock: '0.0.0.0/0',
+      gatewayId: internetGateway.ref,
+    })
+    new CfnSubnetRouteTableAssociation(this, 'PublicSubnetIngress1ARouteTableAssociation', {
+      routeTableId: publicRouteTable.ref,
+      subnetId: pubSubentIngress1A.ref
+    })
+    new CfnSubnetRouteTableAssociation(this, 'PublicSubnetIngress1CRouteTableAssociation', {
+      routeTableId: publicRouteTable.ref,
+      subnetId: pubSubentIngress1C.ref
+    })
+    new CfnSubnetRouteTableAssociation(this, 'PublicSubnetManagement1ARouteTableAssociation', {
+      routeTableId: publicRouteTable.ref,
+      subnetId: pubSubentManagement1A.ref
+    })
+    new CfnSubnetRouteTableAssociation(this, 'PublicSubnetManagement1CRouteTableAssociation', {
+      routeTableId: publicRouteTable.ref,
+      subnetId: pubSubentManagement1C.ref
+    })
+
+    // Application RouteTable
+    const privateAppTable = new CfnRouteTable(this, 'PrivateAppRouteTable', {
+      vpcId: vpc.vpcId,
+    })
+    new CfnSubnetRouteTableAssociation(this, 'PrivateSubnetApp1ARouteTableAssociation', {
+      routeTableId: privateAppTable.ref,
+      subnetId: priSubnetApp1A.ref
+    })
+    new CfnSubnetRouteTableAssociation(this, 'PrivateSubnetApp1CRouteTableAssociation', {
+      routeTableId: privateAppTable.ref,
+      subnetId: priSubnetApp1C.ref
+    })
+
+    // DB RouteTable
+    const privateDBTable = new CfnRouteTable(this, 'PrivateDBRouteTable', {
+      vpcId: vpc.vpcId,
+    })
+    new CfnSubnetRouteTableAssociation(this, 'PrivateSubnetDB1ARouteTableAssociation', {
+      routeTableId: privateDBTable.ref,
+      subnetId: priSubnetDB1A.ref
+    })
+    new CfnSubnetRouteTableAssociation(this, 'PrivateSubnetDB1CRouteTableAssociation', {
+      routeTableId: privateDBTable.ref,
+      subnetId: priSubnetDB1C.ref
     })
   }
 }
